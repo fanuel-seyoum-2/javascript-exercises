f539367c61bedc8924d5349098624d4ce8feaab8
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-09 21:09:07 +0300

    Modify repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(stringE, num) {
+	let newString = "";
 
+	for (let i = 0; i < num; i++)
+		newString += stringE;
+
+	return newString;
 };
 
 // Do not edit below this line
