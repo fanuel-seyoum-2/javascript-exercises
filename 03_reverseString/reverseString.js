2ea8bddc756aa1c757345c25bccc2c55da82135f
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-09 21:24:33 +0300

    Modify reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringE) {
+	
+	let revString = "";
 
+	for (let i = stringE.length - 1; i >= 0; i--)
+		revString += stringE[i];
+
+	return revString;
 };
 
 // Do not edit below this line
