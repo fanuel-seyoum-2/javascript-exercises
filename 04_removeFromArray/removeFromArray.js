c70c2a7967083ec94b99cbb80f7d1653d23e7b7e
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-10 18:36:16 +0300

    Remove elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rems) {
+
+	for (let i = arr.length - 1; i >= 0; i--) {
+		for (let rem of rems) {
+			if (arr[i] === rem){
+				delete arr[i];
+			}
+		}
+	}
+	return arr.filter((i) => i);
 };
 
 // Do not edit below this line
