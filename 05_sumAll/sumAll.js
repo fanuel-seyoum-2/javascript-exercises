f561abcc9303986773652b43c0c882a6bce134fb
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-10 20:22:38 +0300

    Return the sum of two positive integers, with input validation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(en, st) {
+	if (typeof en !== "number" || typeof st !== "number")
+		return "ERROR";
+
+	if (en < 0 || st < 0)
+		return "ERROR";
+	
+	if (!(Number.isInteger(en) && Number.isInteger(st))) 
+		return "ERROR";
+	
+	if  (en > st) {
+		let tmp = en;
+		en = st;
+		st = tmp;
+	}
+
+	let sum = 0;
+	for (let i = st; i >= en; i--)
+		sum += i;
+	return sum;
+	
 
 };
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
