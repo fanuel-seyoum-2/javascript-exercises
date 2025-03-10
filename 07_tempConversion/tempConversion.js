dd6c195c2939bdddd2bb6d46e218232f244273d6
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-10 20:45:44 +0300

    Convert temperature to desired scale

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFah) {
+	return +((5/9) * (tempInFah - 32)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+	return +((9/5) * (tempInCel) + 32).toFixed(1);
 };
 
 // Do not edit below this line
