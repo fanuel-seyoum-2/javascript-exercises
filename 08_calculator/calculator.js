9f5344ba9876f8cb4e61060ecb8a8da3aef1d10d
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-25 10:38:18 +0300

    Complete functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(...nums) {
+	return nums.flat().reduce((total, nums) => total += nums, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+	return nums.flat().reduce((total, nums) => total *= nums, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	for (let i = n - 1; i > 0; --i)
+		n *= i;
+	return (n || 1);
 };
 
 // Do not edit below this line
