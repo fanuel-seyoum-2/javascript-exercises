e89cc11c03961b1ab976b85f442af33c45ee5bfe
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-25 11:06:11 +0300

    Complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+	for (let arrItm of arr) {
+		arrItm.age = (arrItm.yearOfDeath || (new Date().getFullYear())) - arrItm.yearOfBirth;
+	}
+
+	let itm = arr[0];;
+
+	for (let i = 1; i < arr.length; i++)
+		if (arr[i].age > itm.age)
+			itm = arr[i];
+
+	return itm;
+		
 };
 
 // Do not edit below this line
