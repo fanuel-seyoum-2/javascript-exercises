a1ae3a711543e6414240048a1175a72797608b25
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-25 10:55:21 +0300

    Get nth number in fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(reach) {
+	let arr = [0, 1];
 
+	if (reach < 0)
+		return "OOPS";
+
+	for (let i = 0; i < reach; i++) {
+		arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+	}
+
+	return arr[arr.length - 2];
 };
 
 // Do not edit below this line
