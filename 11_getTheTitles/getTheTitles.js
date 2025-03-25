7992de41491133db33ed119ea72c681932d523e6
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-25 10:58:49 +0300

    Complete function getTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+	let titles = [];
 
+	for (arrItm of arr) {
+		if (arrItm.title) 
+			titles.push(arrItm.title);
+	}	
+
+	return titles;
 };
 
 // Do not edit below this line
