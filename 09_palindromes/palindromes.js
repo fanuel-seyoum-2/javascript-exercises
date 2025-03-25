3051ca76211743a101c919397d38f924f144ee6e
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-25 10:45:17 +0300

    Add palindrome checking functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let alNum = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+	string = string
+		.toLowerCase()
+		.split('')
+		.filter((i) => alNum.indexOf(i) >= 0);
+	
+	for (let i = 1; i < string.length / 2; i++) {
+		if (string[i - 1] !== string[string.length - i])
+			return false;
+	}
+	return true;
+		
 };
 
 // Do not edit below this line
